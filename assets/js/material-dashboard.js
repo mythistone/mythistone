8168d2061298560f472970009924a3555af1066e
Jodsderechte <39654549+Jodsderechte@users.noreply.github.com>
2025-09-12 14:09:54 +0200

    fix navigation on mobile fixes #7

--- assets/js/material-dashboard.js
+++ assets/js/material-dashboard.js
@@ -455,13 +455,13 @@ function toggleSidenav() {
   if (body.classList.contains(className)) {
     body.classList.remove(className);
     setTimeout(function() {
-      sidenav.classList.remove('bg-white');
+      sidenav.classList.remove('bg-body');
     }, 100);
     sidenav.classList.remove('bg-transparent');
 
   } else {
     body.classList.add(className);
-    sidenav.classList.add('bg-white');
+    sidenav.classList.add('bg-body');
     sidenav.classList.remove('bg-transparent');
     iconSidenav.classList.remove('d-none');
   }
